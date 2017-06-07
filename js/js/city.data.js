caa5190def19bd6d5905ec435dabf3e87304c468
21克的爱情 <raohong07@163.com>
2017-06-07 18:21:33 +0800

    增加页面，完成下单页面

---
+++ js/js/city.data.js
@@ -0,0 +1,55 @@
+var cityData = [{
+	value: '0',
+	text: '安装',
+	children: [{
+		value: "01",
+		text: "高炮安装"
+	}, {
+		value: "02",
+		text: "橱窗安装"
+	}, {
+		value: "03",
+		text: "展柜安装"
+	},{
+		value: "04",
+		text: "店铺测量"
+	}, {
+		value: "05",
+		text: "店铺测量"
+	}, {
+		value: "06",
+		text: "展柜端头"
+	}, {
+		value: "07",
+		text: "会展安装"
+	}, {
+		value: "08",
+		text: "现场活动"
+	}]
+}, {
+	value: '1',
+	text: '维修',
+	children: [{
+		value: "01",
+		text: "水电维修"
+	}, {
+		value: "02",
+		text: "家具维修"
+	}, {
+		value: "03",
+		text: "电脑维修"
+	}, {
+		value: "04",
+		text: "灯具维修"
+	}, {
+		value: "05",
+		text: "地砖翻新"
+	}]
+}, {
+	value: '2',
+	text: '测量',
+	children: [{
+		value: "01",
+		text: "店铺测量"
+	}]
+}]
